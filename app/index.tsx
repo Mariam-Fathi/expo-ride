0868ef3a03940b764ca61460f881449d30c2fd58
Mariam Fathi <mariam.f.siam@gmail.com>
2025-02-03 19:13:02 +0200

    implement authentication and protect routes

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,10 @@
+import { useAuth } from "@clerk/clerk-expo";
 import { Redirect } from "expo-router";
 
 const Page = () => {
-  // return <Redirect href="/(root)/(tabs)/home" />;
+  const { isSignedIn } = useAuth();
+
+  if (isSignedIn) return <Redirect href="/(root)/(tabs)/home" />;
 
   return <Redirect href="/(auth)/welcome" />;
 };
